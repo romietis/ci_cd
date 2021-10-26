dd7475d53a0593e6efb55c33e12a501f0ed5f3d2
Romans Kotikovs <kotikovsr@wpptechs-MacBook-Pro-2.local>
2021-10-26 19:39:27 +0200

    initial push"

---
+++ app/functions.py
@@ -0,0 +1,6 @@
+def add_1(number):
+    return number + 1
+
+
+if __name__ == '__main__':
+    print(add_1(4))
---
+++ tests/unit/test.py
@@ -0,0 +1,11 @@
+import unittest
+from app import functions
+
+
+class FunctionsTest(unittest.TestCase):
+    def test_add_1(self):
+        self.assertEqual(functions.add_1(4), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
