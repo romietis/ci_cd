c4abe820659ab5837cf75f21148ca1a0d293b61d
romietis <romietis007@gmail.com>
2021-10-27 14:47:36 +0200

    unrigged test

--- tests/unit/test.py
+++ tests/unit/test.py
@@ -5,7 +5,7 @@ from app import functions
 
 class FunctionsTest(unittest.TestCase):
     def test_add_1(self):
-        self.assertEqual(functions.add_1(4), 4)
+        self.assertEqual(functions.add_1(4), 5)
 
 
 if __name__ == '__main__':
