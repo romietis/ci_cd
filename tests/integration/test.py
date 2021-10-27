f0c10cec8a96f9e7f69e9bfdd62f27cf212c918e
romietis <romietis007@gmail.com>
2021-10-27 15:24:47 +0200

    another rigged test

--- tests/integration/test.py
+++ tests/integration/test.py
@@ -8,6 +8,9 @@ class FunctionsTestIntegration(unittest.TestCase):
         with self.assertRaises(TypeError):
             functions.add_1('test')
 
+    def test_bad_test(self):
+        self.assertIsInstance(functions.add_1(5.5), int)
+
 
 if __name__ == '__main__':
     unittest.main()
