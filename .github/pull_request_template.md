3f466d932c184536c21348f014f867db8d8197e6
romietis <romietis007@gmail.com>
2021-11-02 13:48:52 +0100

    all triggers are created by tf, dockerimage using python:alpine, configured build.yaml files to use python3.8., modified gitignore some of the tf files. "

---
+++ .github/pull_request_template.md
@@ -0,0 +1,34 @@
+## PR Type
+
+<!-- Please check the one that applies to this PR using "x". Choose only one -->
+
+- [ ] Bugfix
+- [ ] Feature
+- [ ] Refactoring (no functional changes, no api changes)
+- [ ] Code style update (formatting, local variables)
+- [ ] Build related changes
+- [ ] Documentation
+- [ ] Other... Please describe:
+
+
+## What is the current behavior?
+<!-- Please describe the current behavior that you are modifying, or link to a relevant issue. -->
+
+Issue Number: N/A
+
+## What is the new behavior?
+
+
+## Does this PR introduce a breaking change?
+
+- [ ] Yes
+- [ ] No
+
+
+<!-- If this PR contains a breaking change, please describe the impact and migration path for existing applications below. -->
+
+## Definition of done
+
+- [ ] Unit tested - if new feature required
+- [ ] Deployed to staging (in same PR)
+- [ ] Documentation updated in README.md (tech) as minimum and depending on the usage confluence (business/cross domain)
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,54 +1,40 @@
-#module "project-services" {
-#  source                      = "terraform-google-modules/project-factory/google//modules/project_services"
-#  version                     = "10.1.1"
-#  project_id                  = var.gcp_project
-#  disable_services_on_destroy = true
-#  disable_dependent_services  = true
-#  activate_apis = [,
-#    "cloudbuild.googleapis.com",
-#    "run.googleapis.com",
-#    "containerscanning.googleapis.com"
-#  ]
-#}
-#
-#resource "google_service_account" "google_analytics_integration_sa" {
-#  account_id   = var.google_analytics_integration_sa
-#  display_name = var.google_analytics_integration_sa_display_name
-#}
-#
-#resource "google_service_account_key" "google_analytics_integration_sa_key" {
-#  service_account_id = google_service_account.google_analytics_integration_sa.name
-#  public_key_type    = "TYPE_X509_PEM_FILE"
-#}
-#
-#resource "google_secret_manager_secret" "google_analytics_auth_secret" {
-#  secret_id = "google-analytics-auth"
-#
-#  replication {
-#    automatic = true
-#  }
-#}
-#
-#resource "google_secret_manager_secret_version" "secret_version" {
-#  secret      = google_secret_manager_secret.google_analytics_auth_secret.id
-#  secret_data = base64decode(google_service_account_key.google_analytics_integration_sa_key.private_key)
-#}
-
-resource "google_project_service" "run_api" {
-  service = "run.googleapis.com"
+module "project-services" {
+  source                      = "terraform-google-modules/project-factory/google//modules/project_services"
+  version                     = "10.1.1"
+  project_id                  = var.gcp_project
+  disable_services_on_destroy = true
+  disable_dependent_services  = true
+  activate_apis = [
+    "containerregistry.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "run.googleapis.com",
+    "containerscanning.googleapis.com"
+  ]
 }
 
-resource "google_cloudbuild_trigger" "build_rigger" {
-  name = "ci-cd-trigger-test"
+resource "google_cloudbuild_trigger" "build_trigger_push" {
+  name = "ci-cd-pr-trigger"
   github {
     owner = "romietis"
-    name = "ci_cd"
+    name  = "ci_cd"
     pull_request {
-      branch = "^main$"
+      branch          = "^main$"
       comment_control = "COMMENTS_DISABLED"
     }
   }
   filename = "pull-request-build.yaml"
+}
+
+resource "google_cloudbuild_trigger" "build_rigger" {
+  name = "ci-cd-push-trigger"
+  github {
+    owner = "romietis"
+    name  = "ci_cd"
+    push {
+      branch          = "^main$"
+    }
+  }
+  filename = "cloudbuild.yaml"
 }
 
 resource "google_cloud_run_service" "run_service_test" {
@@ -62,35 +48,5 @@ resource "google_cloud_run_service" "run_service_test" {
       }
     }
   }
-#  traffic {
-#    percent         = 100
-#    latest_revision = true
-#  }
-
-  depends_on = [google_project_service.run_api]
+  depends_on = [module.project-services]
 }
-#
-#resource "google_cloud_run_service_iam_member" "run_all_users" {
-#  service  = google_cloud_run_service.run_service_test.name
-#  location = google_cloud_run_service.run_service_test.location
-#  role     = "roles/run.invoker"
-#  member   = "allUsers"
-#}
-
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location    = google_cloud_run_service.run_service_test.location
-  project     = google_cloud_run_service.run_service_test.project
-  service     = google_cloud_run_service.run_service_test.name
-
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
-
